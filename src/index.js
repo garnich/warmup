34707e00efa9cf5e78060d4b59ade0d312bab7da
garnich <garnich@tut.by>
2018-09-10 22:27:30 +0300

    'second'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 module.exports = function warmup(temperature) {
   // your implementation here
-var temperature;
-temperature = temperature * 1.8 + 32;
-return temperature;
+return temperature * 1.8 + 32;
 };
